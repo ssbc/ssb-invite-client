7a7d4962a161224a284db58b1d493f39a64b329a
Andre Staltz <andre@staltz.com>
2022-03-01 23:20:42 +0200

    replace explain-error with clarify-error

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import run = require('promisify-tuple');
 import {plugin, muxrpc} from 'secret-stack-decorators';
-const explain = require('explain-error');
+const clarify = require('clarify-error');
 const Ref = require('ssb-ref');
 
 interface InviteObject {
@@ -78,18 +78,18 @@ class invite {
 
     // parse the code
     const [e0, parsed] = this.parseInvite(invite);
-    if (e0) return cb(explain(e0, 'Could not accept invalid invite code'));
+    if (e0) return cb(clarify(e0, 'Could not accept invalid invite code'));
     if (!parsed) return cb(new Error('Could not parse invite code: ' + invite));
 
     // connect via SSB CONN
     const addr = parsed.remote;
     const connData = {type: 'pub', autoconnect: true};
     const [e1, rpc] = await run<any>(this.ssb.conn.connect)(addr, connData);
-    if (e1) return cb(explain(e1, 'Could not connect to pub'));
+    if (e1) return cb(clarify(e1, 'Could not connect to pub'));
 
     // command the peer to follow me
     const [e2] = await run(rpc.invite.use)({feed: this.ssb.id});
-    if (e2) return cb(explain(e2, 'Invite not accepted by the pub'));
+    if (e2) return cb(clarify(e2, 'Invite not accepted by the pub'));
 
     if (shouldPublish) {
       // follow the peer
@@ -99,19 +99,19 @@ class invite {
         autofollow: true,
         contact: parsed.key,
       });
-      if (e3) return cb(explain(e3, 'Unable to follow friend behind invite'));
+      if (e3) return cb(clarify(e3, 'Unable to follow friend behind invite'));
 
       // announce the pub to my friends
       const [e4] = await run(this.ssb.publish)({
         type: 'pub',
         address: parsed,
       });
-      if (e4) return cb(explain(e4, 'Unable to announce pub to my friends'));
+      if (e4) return cb(clarify(e4, 'Unable to announce pub to my friends'));
     }
 
     // remember in SSB CONN
     const [e5] = await run(this.ssb.conn.remember)(addr, connData);
-    if (e5) return cb(explain(e5, 'Could not store the pub in ssb-conn'));
+    if (e5) return cb(clarify(e5, 'Could not store the pub in ssb-conn'));
 
     cb(null, true);
   };
